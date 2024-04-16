f41fd03738a14d5413758ddc6ac14a24df11b78d
Risheek <risheek2627.com>
2024-04-16 21:38:57 +0530

    map added

---
+++ JavaScript/Arrays/map.js
@@ -0,0 +1,6 @@
+// todo --------- MAP  ----------
+//  it is also like forEach but difference is map creates new array
+let arr = [2, 3, 4, 5, 6, 7];
+arr.map((val) => {
+  console.log(val * 2);
+});
